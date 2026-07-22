8b23b7deac394436f30d373d55cbd75e191760d0
MyroslavShendrik <citrus.user2204@gmail.com>
2026-07-22 14:57:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -50,17 +50,57 @@ import ReactDOM from 'react-dom/client'; //! 2 - Render React-елемента
 // root.render(element);
 
 //! 3 - Рендер кількох React-елементів
-const elem1 = React.createElement("span", { children: "Привіт " });
-const elem2 = React.createElement("span", { children: "світ!" });
-console.log("elem1:",elem1);
-console.log("elem2:",elem2);
+// const elem1 = React.createElement("span", { children: "Привіт " });
+// const elem2 = React.createElement("span", { children: "світ!" });
+//todo: Each child in a list should have a unique "key" prop
+// const elem1 = React.createElement("span", { key: "elem-1", children: "Привіт " });
+// const elem2 = React.createElement("span", { key: "elem-2", children: "світ!" });
 
-const element = React.createElement("div", {
-  a: 5,
-  b: 10,
-  // children: ["Привіт ", " ", "світ!"]
-  children: [elem1, " ", elem2]
-});
+// console.log("elem1:",elem1);
+// console.log("elem2:",elem2);
+
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   // children: ["Привіт ", " ", "світ!"]
+//   children: [elem1, " ", elem2]
+// });
+// console.log("element:", element);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+
+
+
+//! 4 - Рендер JSX-елемента (XML-образний синтаксис)
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   children: "Привіт, світ!",
+// });
+
+// console.log("element:", element);
+
+// const jsxElement = <div>Привіт, мій світ</div>
+// console.log("jsxElement:", jsxElement);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(element);
+// root.render(jsxElement);
+
+//! 5 - Рендер Вкладених JSX-елементів (інтерполяція)
+const elem1 = <span>Привіт  </span>;
+const elem2 = <span>світ!</span>;
+
+ console.log("elem1:",elem1);
+ console.log("elem2:",elem2);
+
+ const element = (
+  <div>
+    {elem1}
+    {elem2}
+  </div>
+);
+
 console.log("element:", element);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(element);
