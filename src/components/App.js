6d2e9f1e05c642f28a4fa18e021cfc3db8971427
MyroslavShendrik <citrus.user2204@gmail.com>
2026-07-28 12:58:05 +0300

    lesson

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,39 @@
-export default  function App({
-    url, 
-    title, 
-    profileUrl, 
-    author, 
-    price, 
-    quantity
-  }) 
-    {
+import paintings from "../json/painting.json";
+console.log("paintings:",paintings);
+const painting = paintings[2]
+
+export default function App({
+  url,
+  title,
+  profileUrl,
+  author,
+  price,
+  quantity,
+}) {
+  function Painting({ url, title, profileUrl, author, price, quantity }) {
+    return (
+      <div>
+        <img src={url} alt={title} width="480" />
+        <h2>{title}</h2>
+        <p>
+          Автор: <a href={profileUrl}>{author}</a>
+        </p>
+        <p>Цена:{price} кредитов</p>
+        <p>Доступность:{quantity} заканчивается или есть в наличии</p>
+        <button type="button">Додати до кошику</button>
+      </div>
+    );
+  }
   return (
-    <div>
-      <img src={url} alt={title} width="480" />
-      <h2>{title}</h2>
-      <p>
-        Автор: <a href={profileUrl}>{author}</a>
-      </p>
-      <p>Цена:{price}  кредитов</p>
-      <p>Доступность:{quantity} заканчивается или есть в наличии</p>
-      <button type="button">Додати до кошику</button>
-    </div>
+    <>
+      <Painting
+        url={painting.url}
+        title={painting.title}
+        author={painting.author.tag}
+        profileUrl={painting.author.url}
+        price={painting.price}
+        quantity={painting.quantity}
+      />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.js";
 import paintings from "./json/painting.json";
 
+
+console.log("paintings:",paintings)
 //! Створюємо Головний компонент всього застосунку - App.js
-const painting = paintings[0];
+// const painting = paintings[0];
 // const painting = paintings[1]
-// const painting = paintings[2]
+const painting = paintings[2]
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //todo: В index.js ми раендеримо тільки <App />❗️❗️❗️
 root.render(
   <App
-    url={painting.url}
-    title={painting.title}
-    author={painting.author.tag}
-    profileUrl={painting.author.url}
-    price={painting.price}
-    quantity={painting.quantity}
+    // url={painting.url}
+    // title={painting.title}
+    // author={painting.author.tag}
+    // profileUrl={painting.author.url}
+    // price={painting.price}
+    // quantity={painting.quantity}
   />,
 );
